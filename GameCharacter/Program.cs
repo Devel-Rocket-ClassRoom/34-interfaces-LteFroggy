952902196d174de1083d334ef76d21dd2b381790
hojong <33508056+LteFroggy@users.noreply.github.com>
2026-03-04 17:34:48 +0900

    GameCharacter 완료

--- GameCharacter/Program.cs
+++ GameCharacter/Program.cs
@@ -1,4 +1,20 @@
+using GameCharacter;
 using System;
 
-// README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+Character character = new Character(new HealthComponent(Character.k_MaxHp), "용사");
+Monster monster = new Monster(new HealthComponent(Monster.k_MaxHp), "슬라임", 5);
+
+Console.WriteLine($"=== 전투 시작 ===");
+Console.WriteLine();
+
+character.Attack(monster);
+monster.Attack(character);
+character.Attack(monster);
+Console.WriteLine();
+
+Console.WriteLine($"=== 고블린 등장 ===");
+monster = new Monster(new HealthComponent(50), "고블린", 10);
+character.Attack(monster);
+monster.Attack(character);
+character.Attack(monster);
+character.Attack(monster);
